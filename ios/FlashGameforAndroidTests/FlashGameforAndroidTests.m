b3d5bf64c11b09f942ffd1002b181cd0f853467a
tanpm <tanpm@mobiletech.vn>
2023-05-15 21:22:43 +0700

    fix: update app name

--- ios/FlashGameforAndroidTests/FlashGameforAndroidTests.m
+++ ios/FlashGameforAndroidTests/FlashGameforAndroidTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppNameTests : XCTestCase
+@interface FlashGameforAndroidTests : XCTestCase
 
 @end
 
-@implementation AppNameTests
+@implementation FlashGameforAndroidTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
