62e2a54b5847ddc5d581328ba4f3393419c32bb8
tanpm <tanpm@mobiletech.vn>
2023-05-15 10:25:19 +0700

    feat: try to reproduce the issue

--- ios/AppNameTests/AppNameTests.m
+++ ios/AppNameTests/AppNameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface IssueProjectTests : XCTestCase
+@interface AppNameTests : XCTestCase
 
 @end
 
-@implementation IssueProjectTests
+@implementation AppNameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
