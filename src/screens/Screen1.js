bda3846e561d46d8f4536deba16bf5d40932574d
tanpm <tanpm@mobiletech.vn>
2023-05-15 09:40:19 +0700

    feat: try to reproduce the issue

--- src/screens/Screen1.js
+++ src/screens/Screen1.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {AppodealAds} from '../services/AppodealAds';
 
 export class Screen1 extends PureComponent {
   render() {
     return (
-      <View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Screen1</Text>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.navigate('Screen2');
+          }}>
+          <Text>Go to Screen2</Text>
+        </TouchableOpacity>
+        <AppodealAds.Banner />
       </View>
     );
   }
--- src/screens/Screen2.js
+++ src/screens/Screen2.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {AppodealAds} from '../services/AppodealAds';
 
 export class Screen2 extends PureComponent {
+  componentDidMount() {
+    console.log('Screen2 componentDidMount');
+    this.loadAds();
+  }
+
+  loadAds = async () => {
+    await AppodealAds.loadInterstitial();
+    AppodealAds.showInterstitial();
+  };
+
   render() {
     return (
       <View>
